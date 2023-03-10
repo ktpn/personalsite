d68e1b93478d63b21af06bba6a60c6d0ff198033
Ky <kypetern@gmail.com>
2023-03-09 23:23:49 -0600

    Added Background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,63 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
     return (
+        <div class="background">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
         <div classname="App">
-            <h1>HOME PAGE</h1>
-            <p>This application will mainly consists of my React projects.</p>
-            <br></br>
-            <p>If there are any questions or tips, please let me know.</p>
-            <p><a href="mailto:kydesignandcode@gmail.com">Contact me</a></p>
-            <br></br>
-            <p>For egg-citing content, check out my <Link to ="/chicken">Chicken Page</Link> &#128020;.</p>
-
+            <div className="homecontent">
+                <h1>HOME PAGE</h1>
+                <p>This application will mainly consists of my React projects.</p>
+                <br></br>
+                <p>If there are any questions or tips, please let me know.</p>
+                <p><a href="mailto:kydesignandcode@gmail.com">Contact me</a></p>
+                <br></br>
+                <p>For egg-citing content, check out my <Link to ="/chicken">Chicken Page</Link> &#128020;.</p>
+            </div>
         </div>
+        </div>
+        
         
         
 
