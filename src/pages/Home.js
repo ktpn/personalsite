55b10c12ff759ed4daed3765c704afc2f517c0f9
Ky <kypetern@gmail.com>
2023-03-09 22:02:24 -0600

    Add contact

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,8 @@ export const Home = () => {
             <h1>HOME PAGE</h1>
             <p>This application will mainly consists of my React projects.</p>
             <br></br>
-            <p>If there are any questions or tips, please let me know</p>
+            <p>If there are any questions or tips, please let me know.</p>
+            <p><a href="mailto:kydesignandcode@gmail.com">Contact me</a></p>
             <br></br>
             <p>For egg-citing content, check out my <Link to ="/chicken">Chicken Page</Link> &#128020;.</p>
 
